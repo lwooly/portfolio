0eae767a9d28d841a9fd0054e412e3a1f25fd2ec
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2024-01-19 20:53:31 +0000

    apostrophes removed

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -36,10 +36,10 @@ const AboutMe = () => {
         Chartered Engineer & Full-Stack Tech
       </Typography>
       <Typography variant="body1" component="p" sx={{ marginTop: "1em" }}>
-        Hi, I'm Lloyd, a Chartered Structural Engineer venturing into full-stack
+        Hi, I&apos;m Lloyd, a Chartered Structural Engineer venturing into full-stack
         technologies. With a solid foundation in engineering, I am now delving
         into the realms of software development, keen on integrating engineering
-        problem solving ability with modern tech stacks. Currently, I'm deeply
+        problem solving ability with modern tech stacks. Currently, I&apos;m deeply
         engaged in learning and applying technologies such as React and Node.js.
         My ambition is to combine my robust engineering background with
         cutting-edge web development, focusing on creating software solutions
